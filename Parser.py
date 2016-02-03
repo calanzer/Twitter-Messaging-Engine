5e7320f683dbc99e8c074148a3bffa7c2a961c66
clanzer <clanzer@luc.edu>
2016-02-03 16:48:35 -0600

    Included a test case that compares the list topic should contain to what our parser is providing us, so far this test passes.

--- Parser.py
+++ Parser.py
@@ -7,6 +7,7 @@ It will also separate words that start with an @ symbol into a differet "person"
 It will separate words that start with a # symbol in a different "topic" list
 """
 
+
 mystring = "@Frank @it #is very #nice to meet you"
 mylist = mystring.split(" ")
 person = []
@@ -25,6 +26,20 @@ for word in mylist:
 print person
 print topic
 print mylist
+
+import unittest
+
+class PrimesTestCase(unittest.TestCase):
+
+    def test_pass(self):
+        self.assertTrue(True)
+
+    def test_list(self):
+        testTopic = ['#is', '#nice']
+        self.assertListEqual(topic, testTopic)
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
