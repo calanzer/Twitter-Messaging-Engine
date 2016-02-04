e96ed5ce957cdde0d309beb1dfee01b203c5663f
clanzer <clanzer@luc.edu>
2016-02-04 13:04:09 -0600

    Wrote more tests to check all of lists to see if they have the right values. All tests pass. Added comments to explain my process.

--- Parser.py
+++ Parser.py
@@ -7,14 +7,21 @@ It will also separate words that start with an @ symbol into a differet "person"
 It will separate words that start with a # symbol in a different "topic" list
 """
 
-
+#Input to the engine, or our 'tweet' I simply declared it here but idealy it would be user input.
 mystring = "@Frank @it #is very #nice to meet you"
+#Takes our string and splits the words into a list
 mylist = mystring.split(" ")
+#Person list contains all words that start with '@'
 person = []
+#Topic list conatins all word that start with '#'
 topic = []
-str1 = "@"
+#This is just an increment for our print function to tell us which word number we are on
 wordnumber = 1
+
 print mylist
+
+#This function will go through the words in our sentence and print them out, it will also sort through
+#the words that start with '@' or '#' and add them to the person or topic list.
 for word in mylist:
     print "This is word %d %s" % (wordnumber,  word)
     wordnumber += 1
@@ -27,14 +34,25 @@ print person
 print topic
 print mylist
 
+
 import unittest
 
+#Unit tests that check to make sure our lists contain the appropriate values. I used the assertListEqual
+#function to compare lists.
 class PrimesTestCase(unittest.TestCase):
 
     def test_pass(self):
         self.assertTrue(True)
 
     def test_list(self):
+        testList = ['@Frank', '@it', '#is', 'very', '#nice', 'to', 'meet', 'you']
+        self.assertListEqual(mylist, testList)
+
+    def test_list_person(self):
+        testPerson = ['@Frank', '@it']
+        self.assertListEqual(person, testPerson)
+
+    def test_list_topic(self):
         testTopic = ['#is', '#nice']
         self.assertListEqual(topic, testTopic)
 
